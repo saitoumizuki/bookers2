7f349496dfa85793f1499afd0d553c9a5b4efbe7
saitoumizuki <yoshihiramizuki@gmail.com>
2018-05-28 15:17:11 +0900

    modified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,21 @@
 class ApplicationController < ActionController::Base
 	rescue_from ActiveRecord::RecordNotFound do |exception|
-    redirect_to :root
+	redirect_to :root
 	end
 
-  	before_action :authenticate_user!, except: [:about, :index]
+	before_action :authenticate_user!, except: [:about, :index]
 	protect_from_forgery with: :exception
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
-	 protected
+	protected
 
 	def configure_permitted_parameters
-	    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
-	    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+		devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
 
-    end
+	end
 
-    def after_sign_in_path_for(resource)
-    	user_path(current_user.id)
+	def after_sign_in_path_for(resource)
+		user_path(current_user.id)
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,8 @@ class UsersController < ApplicationController
 
 	def new
 		@user = current_user
-    	@users = User.all
-    	@book = Book.new
+		@users = User.all
+		@book = Book.new
 	end
 
 	def edit
@@ -20,18 +20,18 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 		if
 			@user.update(user_params)
-	    	redirect_to user_path(@user.id)
-	    else
-	    	flash[:notice] = '名前は2文字以上、20文字以内・自己紹介は50文字以内にしてください。'
-	    	render :edit
-	    end
+			redirect_to user_path(@user.id)
+		else
+			flash[:notice] = '名前は2文字以上、20文字以内・自己紹介は50文字以内にしてください。'
+			render :edit
+		end
 	end
 
 
 
 	private
 	def user_params
-    	params.require(:user).permit(:name, :introduction, :profile_image)
+		params.require(:user).permit(:name, :introduction, :profile_image)
 	end
 	def correct_user
 		@user = User.find(params[:id])
